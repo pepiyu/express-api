d3ec88da1151d717958c67642700afb57f7b9b7b
pepiyu <58489884+pepiyu@users.noreply.github.com>
2022-06-12 13:30:43 +0200

    test heroku

--- config/session.config.js
+++ config/session.config.js
@@ -10,7 +10,6 @@ const session = expressSession({
   resave: false,
   cookie: {
     httpOnly: true,
-    sameSite: 'none',
     maxAge: process.env.SESSION_MAX_AGE || 3600000000,
   },
   store: new MongoStore({
